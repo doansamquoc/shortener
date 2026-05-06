81cd7ccfd658e9a370a2c0594dd3365d55922e0e
Sam <doansamquoc@gmail.com>
2026-05-07 00:16:36 +0700

    refactor: renamed to SQLFunctionContributor and refactor tsvector to word_similarity search

--- src/main/java/dev/sam/shortener/config/SQLFunctionContributor.java
+++ src/main/java/dev/sam/shortener/config/SQLFunctionContributor.java
@@ -4,14 +4,14 @@ import org.hibernate.boot.model.FunctionContributions;
 import org.hibernate.boot.model.FunctionContributor;
 import org.hibernate.type.StandardBasicTypes;
 
-public class PostgresFTSFunctionContributor implements FunctionContributor {
+public class SQLFunctionContributor implements FunctionContributor {
 	@Override
 	public void contributeFunctions(FunctionContributions contributions) {
 		// Search
 		contributions.getFunctionRegistry().registerPattern(
-			"fts_match",
-			"?1 @@ websearch_to_tsquery('simple',?2)",
-			contributions.getTypeConfiguration().getBasicTypeRegistry().resolve(StandardBasicTypes.BOOLEAN)
+			"word_similarity",
+			"word_similarity(?1, ?2)",
+			contributions.getTypeConfiguration().getBasicTypeRegistry().resolve(StandardBasicTypes.DOUBLE)
 		);
 
 		// Sort
