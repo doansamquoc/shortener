f8af03778d09621537e739b3f0e3b4097319d913
Sam <doansamquoc@gmail.com>
2026-05-07 02:46:32 +0700

    feat: shuffle the ALPHABET

--- src/main/java/dev/sam/shortener/constant/AppConstant.java
+++ src/main/java/dev/sam/shortener/constant/AppConstant.java
@@ -1,6 +1,6 @@
 package dev.sam.shortener.constant;
 
 public class AppConstant {
-	public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234564789";
+	public static final String ALPHABET = "w7PjY8v9m2u1ZxLp5G0QWfE6S4kIbBcD3hA1rTzN8nMoKyR9tV7gX6iU4sO2J3lHq";
 	public static final String V1 = "/api/v1";
 }
