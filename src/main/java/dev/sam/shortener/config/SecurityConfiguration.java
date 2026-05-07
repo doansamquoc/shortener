b24006d692429c5e8c9210b9e07832d955fcb268
Sam <doansamquoc@gmail.com>
2026-05-08 04:26:33 +0700

    chore: remove unused import statement

--- src/main/java/dev/sam/shortener/config/SecurityConfiguration.java
+++ src/main/java/dev/sam/shortener/config/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import dev.sam.shortener.config.jwt.JwtConverter;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -15,7 +14,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static dev.sam.shortener.constant.EndpointConstant.PUBLIC_ENDPOINTS;
