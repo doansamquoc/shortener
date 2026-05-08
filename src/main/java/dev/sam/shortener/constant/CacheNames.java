1b9bfb0c1c2db6801820885ad8f2b770d4bd1ea9
Sam <doansamquoc@gmail.com>
2026-05-08 15:53:56 +0700

    fix: add endpoint version to public endpoints; type missing the TOKEN_BLACKLIST CacheNames

--- src/main/java/dev/sam/shortener/constant/CacheNames.java
+++ src/main/java/dev/sam/shortener/constant/CacheNames.java
@@ -3,4 +3,5 @@ package dev.sam.shortener.constant;
 public class CacheNames {
 	public static final String URL_SHORT = "url:short";
 	public static final String URL_DETAIL = "url:detail";
+	public static final String JWT_BLACKLIST = "jwt:blacklist:";
 }
--- src/main/java/dev/sam/shortener/constant/EndpointConstant.java
+++ src/main/java/dev/sam/shortener/constant/EndpointConstant.java
@@ -2,7 +2,7 @@ package dev.sam.shortener.constant;
 
 public class EndpointConstant {
 	public static final String V1 = "/api/v1";
-	public static final String[] PUBLIC_ENDPOINTS = {"/", "/auth/**"};
+	public static final String[] PUBLIC_ENDPOINTS = {"/", V1 + "/auth/**"};
 	public static final String[] SWAGGER_ENDPOINTS = {"/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html"};
 
 }
