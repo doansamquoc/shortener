efde1370a1a16d79a6cea353a8eef1780872fcc5
Sam <doansamquoc@gmail.com>
2026-05-16 07:17:58 +0700

    fix: display error message clearly

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -11,55 +11,55 @@ const urlFeedBack = document.getElementById("urlFeedBack");
 const copiedToast = document.getElementById("copiedToast");
 
 document.getElementById("shortenForm").addEventListener("submit", async (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  btn.disabled = true;
-  spinner.classList.remove("d-none");
-  icon.classList.add("d-none");
+    btn.disabled = true;
+    spinner.classList.remove("d-none");
+    icon.classList.add("d-none");
 
-  url.classList.remove("is-invalid");
+    url.classList.remove("is-invalid");
 
-  const value = url.value.trim();
+    const value = url.value.trim();
 
-  try {
-    const res = await shortenUrl(value);
-    const json = await res.json();
+    try {
+        const res = await shortenUrl(value);
+        const json = await res.json();
 
-    if (res.ok) {
-      const shortenedUrl = BASE_URL + "/" + json.data.shortenedUrl;
-      document.getElementById("shortenedUrl").value = shortenedUrl;
+        if (res.ok) {
+            const shortenedUrl = BASE_URL + "/" + json.data.shortenedUrl;
+            document.getElementById("shortenedUrl").value = shortenedUrl;
 
-      const shortenedRedirect = document.getElementById("shortenedRedirect");
-      shortenedRedirect.href = shortenedUrl;
-      shortenedRedirect.textContent = shortenedUrl;
+            const shortenedRedirect = document.getElementById("shortenedRedirect");
+            shortenedRedirect.href = shortenedUrl;
+            shortenedRedirect.textContent = shortenedUrl;
 
-      document.getElementById("btnCopy").addEventListener("click", async () => {
-        await navigator.clipboard.writeText(shortenedUrl);
-        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(copiedToast);
-        toastBootstrap.show();
-      });
+            document.getElementById("btnCopy").addEventListener("click", async () => {
+                await navigator.clipboard.writeText(shortenedUrl);
+                const toastBootstrap = bootstrap.Toast.getOrCreateInstance(copiedToast);
+                toastBootstrap.show();
+            });
 
-      const mySuccessModal = new bootstrap.Modal(document.getElementById("successModal"));
-      mySuccessModal.show();
-    } else {
-      url.classList.add("is-invalid");
-      urlFeedBack.textContent = json.message;
+            const mySuccessModal = new bootstrap.Modal(document.getElementById("successModal"));
+            mySuccessModal.show();
+        } else {
+            url.classList.add("is-invalid");
+            urlFeedBack.textContent = json.error.message;
+        }
+    } catch (err) {
+        console.error(err);
+        url.classList.add("is-invalid");
+        urlFeedBack.textContent = err.message;
+    } finally {
+        btn.disabled = false;
+        spinner.classList.add("d-none");
+        icon.classList.remove("d-none");
     }
-  } catch (err) {
-    console.error(err);
-    url.classList.add("is-invalid");
-    urlFeedBack.textContent = err.message;
-  } finally {
-    btn.disabled = false;
-    spinner.classList.add("d-none");
-    icon.classList.remove("d-none");
-  }
 });
 
 async function shortenUrl(url) {
-  return await fetch("http://localhost:8080/api/v1/urls", {
-    method: "POST",
-    headers: new Headers({ "Content-Type": "application/json" }),
-    body: JSON.stringify({ actualUrl: url }),
-  });
+    return await fetch("http://localhost:8080/api/v1/urls", {
+        method: "POST",
+        headers: new Headers({"Content-Type": "application/json"}),
+        body: JSON.stringify({actualUrl: url}),
+    });
 }
